a6938e8869626967eefcb1a76f0931e1795451c9
Tom Van Looy <tom@ctors.net>
2013-06-24 22:27:51 +0200

    unused use

--- web/index.php
+++ web/index.php
@@ -3,7 +3,6 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Intracto\Cron\CronExpression;
 
 $app = new Silex\Application();
